8bd3d48d708c5080e81ecadd32f7b04531818a8a
abdul488 <66771397+abdul488@users.noreply.github.com>
2022-08-02 20:06:21 +0530

    Add files via upload

--- bestfit.c
+++ bestfit.c
@@ -1,46 +1,52 @@
 #include<stdio.h>
- 
+#define max 25
 void main()
 {
-int fragment[20],b[20],p[20],i,j,nb,np,temp,lowest=9999;
-static int barray[20],parray[20];
-printf("\n\t\t\tMemory Management Scheme - Best Fit");
-printf("\nEnter the number of blocks:");
+int frag[max],b[max],f[max],i,j,nb,nf,temp,lowest=10000;
+static int bf[max],ff[max];
+printf("\nMemory Management scheme-best fit");
+printf("\nEnter the number of blocks: ");
 scanf("%d",&nb);
-printf("Enter the number of processes:");
-scanf("%d",&np);
-printf("\nEnter the size of the blocks:-\n");
+printf("Enter the number of files: ");
+scanf("%d",&nf);
+printf("\nEnter the size of blocks \n");
 for(i=1;i<=nb;i++)
-    {
-printf("Block no.%d:",i);
-        scanf("%d",&b[i]);
-    }
-printf("\nEnter the size of the processes :-\n");
-for(i=1;i<=np;i++)
-    {
-        printf("Process no.%d:",i);
-        scanf("%d",&p[i]);
-    }
-for(i=1;i<=np;i++)
+{
+printf("Block %d: ",i);
+scanf("%d",&b[i]);
+}
+printf("Enter the size of files \n");
+for(i=1;i<=nf;i++)
+{
+printf("File %d: ",i);
+scanf("%d",&f[i]);
+}
+for(i=1;i<=nf;i++)
 {
 for(j=1;j<=nb;j++)
 {
-if(barray[j]!=1)
+if(bf[j]!=1)
 {
-temp=b[j]-p[i];
+temp=b[j]-f[i];
 if(temp>=0)
 if(lowest>temp)
 {
-parray[i]=j;
+ff[i]=j;
 lowest=temp;
 }
 }
 }
-fragment[i]=lowest;
-barray[parray[i]]=1;
+frag[i]=lowest;
+bf[ff[i]]=1;
 lowest=10000;
 }
-printf("\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment");
-for(i=1;i<=np && parray[i]!=0;i++)
-printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,p[i],parray[i],b[parray[i]],fragment[i]);
+printf("\nFile no\t\tFile size\t\tBlock size\t\tFragment");
+for(i=1;i<=nf;i++)
+{
+if(ff[i]!=0)
+printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
+else
+printf("\n%d\t\t%d\t\tNot Allocated",i,f[i]);
+}
+printf("\n");
 }
